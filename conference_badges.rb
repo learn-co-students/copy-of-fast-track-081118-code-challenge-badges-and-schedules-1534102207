817b8b0701703cb1cf8567e71e6bb50b028d303e
Daniel Loyd <dhloyd@icloud.com>
2018-08-12 20:33:33 +0000

    Finished-Dloyd1

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+namearray = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def batch_badge_creator(namearray)
+namearray.map do |name|
+puts "Hello, my name is #{name}."
+end
+end
+
+def assign_rooms(namearray)
+counter = 1
+namearray.map do |name|
+puts "Hello, #{name}! You will be assigned to room #{counter}."
+counter = counter + 1
+end
+end
+
+def print(namearray)
+batch_badge_creator(namearray)
+assign_rooms(namearray)
+end 
+
+print(namearray)
